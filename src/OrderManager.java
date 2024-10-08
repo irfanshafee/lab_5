56a5eb9d34ae613f311acb52f7740130a9fab240
irfanshafee <irfanshafee1@gmail.com>
2024-10-08 11:40:02 +0600

    completed order class

---
+++ src/OrderManager.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.Comparator;
+
+public class OrderManager {
+    public List<Product> sortProducts(List<Product>products){
+
+        products.sort(Comparator.comparingDouble(Product:: calculateTotalPrice)
+                                .thenComparing(Product:: getName)
+                                  .thenComparingInt(Product:: getStockQuantity));
+                                        return products;
+
+    }
+
+}
